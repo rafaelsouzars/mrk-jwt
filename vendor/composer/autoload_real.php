d038840c4b02f4412f55a76a57fba33ac786937d
rafaelsouzars <rafael21.santos@hotmailcom>
2024-03-24 10:57:09 -0300

    :wrench: chore: Ajustes

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit893c922534f3ea087bf6f569215eb126
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit893c922534f3ea087bf6f569215eb126', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit893c922534f3ea087bf6f569215eb126', 'loadClassLoader'));
